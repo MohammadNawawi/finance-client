14441ff92fe5c46931f82b12a3af34cf7cf54e13
Mohammad Nawawi <tikp2.e24m.nawawi@gmail.com>
2023-04-02 13:06:37 +0700

    React Router & Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
+import NavBar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        Hi
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard</div>} />
+              <Route path="/predictions" element={<div>Predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
